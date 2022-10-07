3cc96ac208247aab7952893b10ed729de0d30022
Catalina F. Mora <catalinafullanamora@gmail.com>
2022-10-07 10:46:09 +0200

    declaración funciones Oca

---
+++ 1-1/FP1/juegoOcaCatalina/juegoOcaCatalina/juegoOca.cpp
@@ -0,0 +1,75 @@
+/*
+* CATALINA FULLANA MORA Y RAFAEL HERNANDO HERIAS
+* Práctica Final Fundamentos de la Programación I
+* 07/10/2022 - 6/11/2022
+* 
+* 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+}
+
+
+bool esOca(int casilla) {
+	
+}
+bool esPuente(int casilla) {
+
+}
+bool esDados(int casilla) {
+
+}
+bool esLaberinto(int casilla) {
+
+}
+bool esMuerte(int casilla) {
+
+}
+bool esPosada(int casilla) {
+
+}
+bool esPrision(int casilla) {
+
+}
+bool esPozo(int casilla) {
+
+}
+bool esMeta(int casilla) {
+
+}
+
+
+int siguienteOca(int casilla){
+	return 0;
+}
+int siguientePuente(int casilla) {
+	return 0;
+}
+int siguienteDado (int casilla){
+	return 0;
+}
+int siguienteLaberinto(int casilla) {
+	return 0;
+}
+int siguienteMuerte(int casilla) {
+	return 0;
+}
+
+int tirarDado() {
+	return 0;
+}
+int quienEmpieza() {
+	return 0;
+}
+int efectoPosicion(int casillaActual) {
+	return 0;
+}
+int efectoTiradas(int casillaActual, int numeroDeTiradas) {
+	return 0;
+}
+
+
