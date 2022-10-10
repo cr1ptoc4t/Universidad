ef4541eec0a616daeffd18257db51b0d0534782d
Catalina F. Mora <catalinafullanamora@gmail.com>
2022-10-10 23:14:47 +0200

    10/10 noche

--- 1-1/FP1/juegoOcaCatalina/juegoOcaCatalina/juegoOca.cpp
+++ 1-1/FP1/juegoOcaCatalina/juegoOcaCatalina/juegoOca.cpp
@@ -15,8 +15,15 @@ const int CASILLA_PUENTE_1 = 6;
 const int CASILLA_PUENTE_2 = 12;
 const int CASILLA_DADOS_1 = 26;
 const int CASILLA_DADOS_2 = 53;
-bool esOca(int casilla);
+const int TURNOS_POSADA = 1;
+const int CASILLA_LABERINTO = 42;
+const int CASILLA_POSADA = 19;
+const int CASILLA_PRISION = 52;
+const int CASILLA_MUERTE = 58;
+const int CASILLA_POZO = 31;
 
+bool esOca(int casilla);
+bool esPuente(int casilla);
 
 int main() {
 	cout << "hay oca en la casilla 65?" << esOca(6) << endl;
@@ -47,30 +54,39 @@ bool esOca(int casilla) {
 }
 
 
-/*
 bool esPuente(int casilla) {
-
+	bool a;
+	if (casilla = CASILLA_PUENTE_1 || casilla == CASILLA_PUENTE_2) {
+		a = true;
+	}
+	else { a = false; }
+	return a;
 }
 bool esDados(int casilla) {
-
+	bool a;
+	if (casilla = CASILLA_DADOS_1 || casilla == CASILLA_DADOS_2) {
+		a = true;
+	}
+	else { a = false; }
+	return a;
 }
 bool esLaberinto(int casilla) {
-
+	return 0;
 }
 bool esMuerte(int casilla) {
-
+	return 0;
 }
 bool esPosada(int casilla) {
-
+	return 0;
 }
 bool esPrision(int casilla) {
-
+	return 0;
 }
 bool esPozo(int casilla) {
-
+	return 0;
 }
 bool esMeta(int casilla) {
-
+	return 0;
 }
 
 
@@ -103,5 +119,3 @@ int efectoTiradas(int casillaActual, int numeroDeTiradas) {
 	return 0;
 }
 
-
-*/
