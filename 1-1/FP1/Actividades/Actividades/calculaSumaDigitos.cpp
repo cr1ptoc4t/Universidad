2502cdc0f69b23ce88393ae21f082d59ae1c6962
Catalina F. Mora <catalinafullanamora@gmail.com>
2022-11-16 16:14:04 +0100

    actividades dia antes examen

--- 1-1/FP1/Actividades/Actividades/calculaSumaDigitos.cpp
+++ 1-1/FP1/Actividades/Actividades/calculaSumaDigitos.cpp
@@ -2,7 +2,18 @@
 #include <fstream>
 using namespace std;
 
+const int LONGITUD_ARRAY1 = 20;
+typedef int tArray[LONGITUD_ARRAY1];
+
 int main() {
+	tArray array1;
+	for (int i = 0; i < LONGITUD_ARRAY1; i++) {
+		array1[i] = i;
+		cout << array1[i] << " | ";
+	}
+
+
+	/*
 	ifstream archivo;
 	archivo.open("datos.txt");
 	int num=1, sumaTotal = 0, sumaDiv;
@@ -27,4 +38,8 @@ int main() {
 	archivo.close();
 	cout << "hay un total de: " << sumaTotal << " numeros perfectos" << endl;
 	return 0;
+
+	*/
 }
+
+
