86f5875dfe896dc851b9ad3f9df4e0649ca62ea4
Catalina F. Mora <catalinafullanamora@gmail.com>
2022-12-25 00:13:16 +0100

    2/3 examen 2

--- 1-1/FP1/Examenes anteriores/Examen 2/Examen 2/examen2.cpp
+++ 1-1/FP1/Examenes anteriores/Examen 2/Examen 2/examen2.cpp
@@ -6,10 +6,24 @@ using namespace std;
 
 const int MAX_JUGUETES= 10, MAX_CARTAS=10, MAX_PEDIDOS =3, MAX_NOMBRES =10;
 
+
+
+struct tJuguetes {
+	string nombre;
+	double peso;
+	int unidades;
+};
+
+struct tListaJuguetes {
+	tJuguetes juguetes[MAX_JUGUETES];
+	int cont;
+};
+
 struct tCartas {
 	string persona;
 	string ciudad;
-	 
+	tListaJuguetes listaJug;
+	
 	bool preparado;
 	bool pendiente;
 };
@@ -19,15 +33,6 @@ struct tListaCartas {
 	int cont;
 };
 
-struct tJuguetes {
-	string nombre;
-	double peso;
-	int unidades;
-};
-struct tListaJuguetes {
-	tJuguetes juguetes[MAX_JUGUETES];
-	int cont;
-};
 //----------------------------------------------------------------------------
 bool cargarJuguetes(tListaJuguetes& lista);
 bool cargarCartas(tListaCartas& lista);
@@ -60,13 +65,13 @@ int main() {
 
 bool cargarJuguetes(tListaJuguetes& lista) {
 	bool cargado = false;
-	string nombre;
+	string nombre = "";
 	ifstream archivo;
 	archivo.open("juguetes.txt");
 	if (archivo.is_open()) {
 		int i = 0;
-		archivo >> nombre;
 		while (i < MAX_JUGUETES && nombre != "XXX") {
+			archivo >> nombre;
 			lista.juguetes[i].nombre = nombre;
 			archivo >> lista.juguetes[i].peso;
 			archivo >> lista.juguetes[i].unidades;
@@ -82,12 +87,33 @@ bool cargarJuguetes(tListaJuguetes& lista) {
 }
 
 bool cargarCartas(tListaCartas& lista) {
+	bool cargado = false;
+	string nombre = "";
+	fstream archivo;
+	archivo.open("cartas.txt");
+	if (archivo.is_open()) {
+		int i = 0;
+		
+		while (i < MAX_CARTAS && nombre != "XXX") {
+			archivo >> lista.listaCartas[i].persona;
+			archivo >> lista.listaCartas[i].ciudad;
+			for (int j = 0; i < MAX_JUGUETES;i++) {
+				archivo >> lista.listaCartas[i].listaJug.juguetes[j].nombre;
+			}
+			i++;
+		}
+		lista.cont = i - 1;
 
+		archivo.close();
+		cargado = true;
+	}
+
+	return cargado;
 }
 
 void listarJuguetes(const tListaJuguetes lista) {
 	cout << "Listado de los juguetes que hay en la fabrica:" << endl;
-	cout << "Nombre \nPeso\nCant." << endl;
+	cout << setw(MAX_NOMBRES) << left << "Nombre" << setw(MAX_NOMBRES) << "Peso" << setw(MAX_NOMBRES) << "Cant." << '\n';
 	for (int i = 0; i < lista.cont-1; i++) {
 		cout << setw(MAX_NOMBRES) << left << lista.juguetes[i].nombre;
 		cout << setw(MAX_NOMBRES) << fixed << setprecision(2) << lista.juguetes[i].peso;
@@ -103,11 +129,11 @@ int buscarJuguete(const string nombre, const tListaJuguetes lista){
 }
 
 double trineoDestino(string& ciudad, tListaCartas& listaCartas, tListaJuguetes& listaJuguetes){
-
+	return 0.1;
 }
 
 bool comprobarPedido(const tListaJuguetes juguetes, string pedido[MAX_PEDIDOS]){
-
+	return true;
 }
 
 void cargarTrineos(tListaCartas& cartas, tListaJuguetes& listaJuguetes){
@@ -115,12 +141,24 @@ void cargarTrineos(tListaCartas& cartas, tListaJuguetes& listaJuguetes){
 }
 
 void listarPendientes(const tListaCartas cartas){
+	cout << endl;
+	cout << "Regalos pendientes: " << endl;
+	cout << setw(MAX_NOMBRES) << left << "Nombre" << setw(MAX_NOMBRES) << "Ciudad" << setw(MAX_NOMBRES) << "Juguetes" << '\n';
 
+	for (int i = 0; i < cartas.cont - 1; i++) {
+		if(cartas.listaCartas[i].pendiente){
+			cout << setw(MAX_NOMBRES) << left << cartas.listaCartas[i].persona;
+			cout << setw(MAX_NOMBRES) << fixed << setprecision(2) << cartas.listaCartas[i].ciudad;
+			for (int j = 0; j < MAX_JUGUETES;j++) {
+				cout << cartas.listaCartas[i].listaJug.juguetes[j].nombre<<"   ";
+			}
+		}
+	}
 }
 
 
 int buscarCiudad(string& ciudad, const tListaCartas& listaCartas) {
-
+	return 1;
 }
 // El programa principal carga los dos ficheros de datos en sus listas.
 // Luego muestra los juguetes de la fábrica, carga los trineos y por último 
