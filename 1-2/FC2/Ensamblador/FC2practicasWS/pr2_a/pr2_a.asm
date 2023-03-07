eb99db5bcfb01ca36c3ff503e5d4345fbc94b1bc
Catalina F. Mora <catalinafullanamora@gmail.com>
2023-03-07 23:45:17 +0100

    practica2

--- 1-2/FC2/Ensamblador/FC2practicasWS/pr2_a/pr2_a.asm
+++ 1-2/FC2/Ensamblador/FC2practicasWS/pr2_a/pr2_a.asm
@@ -35,25 +35,35 @@ min: .space 4
 .text
 main:
 	li t1, N		//t1=N
-    la s0, min
-    li t0, INT_MAX
-    sw t0, 0(s0)
+    la s5, min		//s5=min
+    li s5, 0
+    li t0, INT_MAX	//t0=INT_MAX
+    sw t0, 0(s5)
     li t2, 0		//t2=i
-
+    //lw s1, V
+	la t3, V
 for:
-	bge t2, t1, fin_for
+	bge t2, t1, fin_for		//si cc, saltar
+	slli t5, t2, 2			//desplazamiento
+	add t5, t5, t3		//t5=t5+t3
+	lw  s3, 0(t5)     	// @s1=v[i]
+
+if:
+	ble s5, s3, fin_if
+	mv s5,s3
+	//sw s3, 0 (s5)		//almacenar el valor de s3 en min
+
+	//min toma valor de s3
+
+fin_if:
 	addi t2, t2, 1 			//incrementar indice
-	//bge s0, v[i], si_menor
-	//si condicion contraria v[i]<min (s0), si_menor
 	j for
 
 fin_for:
+	la s3, min
+	la s5, min //resultado en s5
 	j end
 
 end:
 	j .
 	.end
-
-si_menor:
-	//min toma valor de v[i]
-	j for
--- 1-2/FC2/Ensamblador/FC2practicasWS/pr2_b/pr2_b.asm
+++ 1-2/FC2/Ensamblador/FC2practicasWS/pr2_b/pr2_b.asm
@@ -43,7 +43,51 @@ min: .space 4
 ind: .space 4
 
 .text
-main :
-// codificar
+main:
+	li t1, N			//t1=N´
+	la s8, ind			//s8=ind=0
+	li s8, 0
+	la s5, min
+	li s5, 0			//min=s5=0
+	li t0, INT_MAX		//t0=INT_MAX
+	li t2, 0			//t2=i
+	li t4, 0			//t4=j
+	la t3, V
+	la t6, W
+
+for:
+	bge t2, t1, fin_for		//si cc, saltar
+	li t4, 0
+segundo_for:
+	bge t4, t1, fin_segundo_for
 
 
+if:
+	slli t5, t4, 2			//desplazamiento
+	add t5, t5, t6			//t5=t5+t3
+	lw  s6, 0(t5)     		// @s1=v[i]
+	ble s5, s3, fin_if
+	mv s5,s3
+	mv s8,t4
+
+fin_if:
+	addi t4, t4, 1
+	j segundo_for
+
+fin_segundo_for:
+
+	slli t5, t2, 2			//desplazamiento
+	add t5, t5, s6			//t5=t5+t6
+	mv s6, t5
+	mv t5, t1
+
+	addi t2, t2, 1 			//incrementar indice
+	j for
+
+
+fin_for:
+
+	j end
+end:
+	j .
+	.end
