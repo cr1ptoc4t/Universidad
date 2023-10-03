d454066ed17f682276af6f9a973f0aea7e52f5bd
Catalina F. Mora <catalinafullanamora@gmail.com>
2023-10-03 11:04:51 +0200

    acepta el reto

--- 2-1/FAL/acepta el reto/entrega5/250/250.cpp
+++ 2-1/FAL/acepta el reto/entrega5/250/250.cpp
@@ -3,20 +3,26 @@ using namespace std;
 
 int main() {
 	int num, act;
-	int v [1000000];
+	int v [100000];
 	cin >> num;
 	while (num != 0) {
+		int dia = 1;
 		int suma = 0;
 		int j = 0;
 		for (int i = 0; i < num; i++)
 			cin >> v[i];
 
-		while (j < num && suma != 0){
+		while (j < num-1 && (suma != 0||j==0)){
 			j++;
 			suma += v[j];
+			dia++;
 		}
+		if (suma == 0)
+			cout << dia << endl;
+		else
+			cout << 0 << endl;
+
 		cin >> num;
-		cout << suma << endl;
 	}
 	return 0;
 }
