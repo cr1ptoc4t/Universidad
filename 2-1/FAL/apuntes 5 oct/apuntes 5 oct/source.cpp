87e203906146ca2a3ebcba19473e6b8fb2744e83
Catalina F. Mora <catalinafullanamora@gmail.com>
2023-10-19 13:52:21 +0200

    FAL

--- 2-1/FAL/apuntes 5 oct/apuntes 5 oct/source.cpp
+++ 2-1/FAL/apuntes 5 oct/apuntes 5 oct/source.cpp
@@ -2,6 +2,11 @@ using namespace std;
 #include <iostream>
 bool crecienteYDivertido(int v[], int n, int d);
 bool quasiFib(int v[], int n, int k);
+void ordenar(int v[], int a, int b);
+int fact(int n);
+int fib(int n);
+void mezcla(int v[], int a, int m, int b);
+
 
 int main() {
 
@@ -38,4 +43,39 @@ bool crecienteYDivertido(int v[], int n, int d) {
 	while (i < n) i++;
 
 	return i == n;
+}
+
+// 19/10
+int fib(int n) {
+	if (n <= 1)
+		return n;
+
+	return fib(n - 1) + fib(n - 2);
+}
+
+int fact(int n) {
+	if (n == 0)
+		return 1;
+	return n * fact(n - 1);
+}
+
+
+//ordenacion de un vector de forma recursiva
+//o(n log n)
+//Merge Sort // quicksort
+void ordenar(int v[], int a, int b) {
+	if (a >= b)
+		return;
+
+	int m = (a + b) / 2;
+	
+	ordenar(v,a, a+m);
+	ordenar(v,a+m,b);
+	mezcla(v,a,m,b);
+}
+
+
+//merge
+void mezcla(int v[], int a, int m, int b) {
+
 }
