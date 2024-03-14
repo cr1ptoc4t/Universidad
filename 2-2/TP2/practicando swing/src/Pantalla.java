5eb1d5d4a1137edccdc2e3f15ea61c8bfd5325ca
Catalina F. Mora <catalinafullanamora@gmail.com>
2024-03-15 00:37:39 +0100

    programa pantalla

--- 2-2/TP2/practicando swing/src/Pantalla.java
+++ 2-2/TP2/practicando swing/src/Pantalla.java
@@ -17,20 +17,43 @@ public class Pantalla extends JFrame implements KeyListener {
 
 
     public void initGUI(){
-        JTextArea saludo = new JTextArea("Hola amorch");
-        JPanel panelPrincipal = new JPanel(new GridLayout());
+        JTextArea saludo = new JTextArea("Hola amorch,\n vas a renunciar a ser un FIFE por mi?");
 
-        JButton botonC = new JButton("Sí");
-        panelPrincipal.add(saludo);
-        panelPrincipal.add(botonC);
+        JPanel panelPrincipal = new JPanel(new BorderLayout());
+        panelPrincipal.setBackground(new Color(255,100,100));
+
+        saludo.setAlignmentX(getWidth()/2);
+
+        JButton botonC = new JButton("nO");
+        JButton botonSi = new JButton("SIIIIII, renunciare al gym, al fútbol, a messi " +
+                ".Alabaré a Taylor Swift por encima de mi cadaver "+
+                "y en caso de fallecer le donaré mi cuerpo para que pueda dominar el mundo " +
+                "y usarme como combustible si tiene que ir a por una " +
+                "barra de pan!!!!!!!!!!");
+        botonC.setBackground(new Color(255, 100,120));
+        botonC.setSize(10, 20);
+        botonC.setPreferredSize(new Dimension(40, 40));
+        botonC.setMaximumSize(new Dimension(40, 40));
+        panelPrincipal.add(saludo, BorderLayout.PAGE_START);
+        panelPrincipal.add(botonSi, BorderLayout.CENTER);
+        panelPrincipal.add(botonC, BorderLayout.PAGE_END);
 
         botonC.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //reubicar boton
+                botonC.setLocation((int)(Math.random()*getWidth()), (int)(Math.random()*getHeight()));
             }
         });
 
+        botonSi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                panelPrincipal.add((new JPanel()).add(new JLabel("OLEEEEEEE")), BorderLayout.CENTER);
+            }
+        });
+
+        panelPrincipal.setSize(1900, 1900);
+        //panelPrincipal.(false);
         this.setContentPane(panelPrincipal);
         //siempre hacer esto, si no no se ve
         this.setVisible(true);
