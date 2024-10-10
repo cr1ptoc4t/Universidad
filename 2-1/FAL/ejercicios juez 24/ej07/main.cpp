8f5543c3f3130938d6a0c11d29693788ac5851be
Catalina F. Mora <catalinafullanamora@gmail.com>
2024-10-10 17:38:39 +0200

    ejercicios fal

---
+++ 2-1/FAL/ejercicios juez 24/ej07/main.cpp
@@ -0,0 +1,94 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int TMAX = 2000000;  // No habr� vectores de m�s de dos millones de elementos
+
+
+/*
+
+PRECONDICION DE LA FUNCION:
+  ---Escribe aqu� la precondici�n de la funci�n.
+  PARA_TODO i : 0<i<TAM(a): a[i]<k && 0<n<TAM(a)
+*/
+
+int llena_mochila(const int a[], int n, int k);
+
+/*
+POSTCONDICION DE LA FUNCION:
+  -- Escribe aqu� la postcondici�n de la funci�n. Para referirte
+  -- al resultado puedes usar la pseudovariable 'resul'
+  
+  max_prod= max{ SUMA MAX DE i A j : i<j y todos los elementos entre ellos son consecutivos
+
+
+ */
+
+
+ /*
+ DISE�O DEL ALGORITMO:
+
+ Paso 1. Variables
+
+
+ Paso 2. Invariante
+
+
+ Paso 3. Inicializaci�n
+
+ Paso 4. Condici�n del bucle, y c�digo tras el bucle.
+
+
+ Paso 5. Cuerpo del bucle
+
+ Paso 6. Justificaci�n de que el algoritmo siempre termina.
+ la ventana se va haciendo pequeña y no existe ningún elemento que pese
+ más que k
+
+ Paso 7. Complejidad: O(n)
+
+
+ */
+
+int llena_mochila(const int a[], int n, int k) {
+	int ini = 0;
+	int fin;
+	int suma_act=0;
+	int max_prod=0;
+	for (int fin = 0; fin < n; fin++) {
+		suma_act += a[fin];
+
+		while (suma_act > k) {
+			suma_act -= a[ini];
+			ini++;
+		}
+
+		if (fin - ini + 1 > max_prod)
+			max_prod = fin - ini + 1;
+	}
+	
+	return max_prod;
+}
+
+
+/* CODIGO PARA LEER Y EJECUTAR LOS CASOS DE PRUEBA */
+
+void lee_vector(int a[], int& n) {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+}
+
+int main() {
+	static int a[TMAX];
+	int n;
+	do {
+		lee_vector(a, n);
+		if (n >= 0) {
+			int k;
+			cin >> k;
+			cout << llena_mochila(a, n, k) << endl;
+		}
+	} while (n != -1);
+}
---
+++ 2-1/FAL/ejercicios juez 24/ej08/main.cpp
@@ -0,0 +1,105 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+/*
+
+PRECONDICION DE LA FUNCION:
+  ---Escribe aqu� la precondici�n de la funci�n.
+  P: { 0<=n<tam(a)
+*/
+
+int tramo_mas_corto(bool a[], int n, int k);
+
+/*
+POSTCONDICION DE LA FUNCION:
+  -- Escribe aqu� la postcondici�n de la funci�n. Para referirte
+  -- al resultado puedes usar la pseudovariable 'resul'
+​
+ 
+
+ */
+
+
+ /*
+ DISE�O DEL ALGORITMO:
+
+ Paso 1. Variables
+
+
+ Paso 2. Invariante
+
+
+ Paso 3. Inicializaci�n
+
+ Paso 4. Condici�n del bucle, y c�digo tras el bucle.
+
+
+ Paso 5. Cuerpo del bucle
+
+ Paso 6. Justificaci�n de que el algoritmo siempre termina.
+	fin avanza en cada iteración y n nunca cambia. en algun momento fin tiene que "superar"
+	al valor de n
+
+ Paso 7. Complejidad:
+ O(n)
+
+ */
+
+
+
+int tramo_mas_corto(bool a[], int n, int k) {
+	// A IMPLEMENTAR
+	int inicio = 0; 
+	int contador = 0;
+	int min_tamano = n + 1;
+
+	for (int fin = 0; fin < n; ++fin) {
+		if (a[fin]) { 
+			contador++;
+		}
+
+		while (contador == k) {
+			min_tamano = min(min_tamano, fin - inicio + 1);
+
+			if (a[inicio]) {
+				contador--; 
+			}
+			inicio++;  
+		}
+	}
+
+	return (min_tamano == n + 1) ? n + 1 : min_tamano;
+}
+
+/* CODIGO DE LECTURA Y EJECUCION DE CASOS DE PRUEBA:
+   NO MODIFICAR */
+
+
+void lee_caso(int& n, bool a[], int& k) {
+	cin >> n;
+	if (n != -1) {
+		for (int i = 0; i < n; i++) {
+			int elem;
+			cin >> elem;
+			a[i] = bool(elem);
+		}
+		cin >> k;
+	}
+}
+
+const int MAX_TAM = 100;
+int main() {
+	int n;
+	bool a[MAX_TAM];
+	int k;
+	do {
+		lee_caso(n, a, k);
+		if (n != -1) {
+			cout << tramo_mas_corto(a, n, k) << endl;
+		}
+	} while (n != -1);
+}
+
